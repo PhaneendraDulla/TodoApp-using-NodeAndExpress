e736b3027e9f5816a7d62a92c299409c23243361
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-10-30 15:29:21 -0400

    Update todo.ts

--- src/Routes/todo.ts
+++ src/Routes/todo.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { createTodo } from "../Controllers/todos";
 
 //const express = require('express');
 //const Router = express.Router;
 
 const router = Router();
 
-router.post("/");
+router.post("/", createTodo);
 
 router.get("/");
 
